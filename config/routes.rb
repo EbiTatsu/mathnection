bc09ed8773fbd1bcb710b07c522670783bdc2c39
EbiTatsu <tatsuya.ebifly888yen@gmail.com>
2022-01-05 08:03:53 +0900

    ユーザー管理機能実装2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # devise_for :users
   root to: "questions#index"
   resources :questions, only: [:new, :create, :show, :edit, :update, :destroy ] do
     resources :comments, only: :create
